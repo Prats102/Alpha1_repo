65389a7632a690110e63c7d1e959fcbf166818bc
pratyush <me.pratyush102@gmil.com>
2018-07-07 20:27:15 +0530

    added alpha1 test

---
+++ src/com/infi/pratyush/alpha1/Alpha1Test.java
@@ -0,0 +1,13 @@
+package com.infi.pratyush.alpha1;
+
+public class Alpha1Test {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
